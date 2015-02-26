31486bd513871ea17c5cb53ac2e479672f7b9a86
Koryphon <Koryphon@users.noreply.github.com>
2015-02-26 11:46:34 +0100

    Added a simple example

---
+++ examples/simpleExample/simpleExample.ino
@@ -0,0 +1,37 @@
+/*
+ * Very simple example of the use of the ScheduleTable library
+ *
+ * This example uses a unique ScheduleTable with one action which prints
+ * the number of execution of the action on Serial.
+ * The period of the schedule table is 1000ms and the action is
+ * set at the end of the period.
+ */
+
+#include <ScheduleTable.h>
+ 
+ScheduleTable helloCycle(1,1000);
+
+void hello()
+{
+  static byte count = 0;
+  
+  Serial.print(++count);
+  Serial.print(" Hello it is ");
+  Serial.println(millis());
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  /* the action is set à 1000ms from start of the period */
+  helloCycle.at(1000,hello);
+  
+  /* start the schedule table for 10 times */
+  helloCycle.start(10);
+}
+
+void loop()
+{
+  ScheduleTable::update();
+}
